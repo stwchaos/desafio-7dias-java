0003fe1c69d05d5e0ab8b4ad030fb91043056e4c
sam <pflegermendes@gmail.com>
2022-10-03 17:20:16 -0300

    Dia 1

---
+++ src/nomes/Nomes.java
@@ -0,0 +1,35 @@
+package nomes;
+
+/**
+ * 
+ * @author Sam Pfleger Mendes
+ * @date 03/10/2022
+ *
+ */
+
+import java.util.Scanner;
+
+public class Nomes {
+
+	public static void main(String[] args) {
+		nomes(null);
+
+	}
+
+	/**
+	 * Ler 3 nomes (digitados pelo usuário) e exibir no console todos eles
+	 * @param nomes
+	 */
+	
+	public static void nomes(String nomes) {
+		Scanner readT = new Scanner(System.in);
+		String nome;
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println("digite um nome");
+			nome = readT.nextLine();
+			System.out.println("o numero digitado foi: " + nome);
+		}
+	}
+
+}
